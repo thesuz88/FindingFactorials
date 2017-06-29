9540558770fb7f3031bc94ab8c233323e96637b4
thesuz88 <stevensuzio@gmail.com>
2017-06-28 21:52:52 -0400

    added comments and cleaned up formatting

--- src/Main.java
+++ src/Main.java
@@ -9,34 +9,40 @@ public class Main {
         String choice = "y";
 
         do {
-            int factorial = 1;
+            long factorial = 1;
             System.out.println("Enter a number between 1 and 10: ");
 
-
-            while (!scan.hasNextInt()){
+            //validates the input to make sure it is an integer
+            while (!scan.hasNextInt()) {
                 System.out.println("Did not enter a number.");
                 System.out.println("Enter a number between 1 and 10");
                 String input = scan.nextLine();
             }
+
             userNum = scan.nextInt();
+
+            // Finds the factorial of the user number
             if (userNum <= 10) {
 
                 for (int i = 1; i <= userNum; i++) {
 
                     factorial = i * factorial;
-
                 }
 
                 System.out.println("The factorial of " + userNum + " is " + factorial);
             }
+
+            //checks to make sure the input is in range
             if (userNum > 10) {
                 System.out.println("Invalid number entered.");
             }
 
-            choice = scan.nextLine();
+            choice = scan.nextLine(); //cleans out scanner
             System.out.println("Continue? (y/n)");
             choice = scan.nextLine();
 
-        } while (choice.equalsIgnoreCase("y"));
+        } while (choice.equalsIgnoreCase("y")); //Iterates again if user types "y" else it quits
+
+        System.out.println("Good bye!");
     }
 }
